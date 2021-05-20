249dc30a6b730f794ce220a90ab8ddb12f2ff735
Inathan <inathan.moreira.10@gmail.com>
2021-05-19 21:55:08 -0300

    Service additions in the book class

--- library/src/main/java/com/library/repositories/BookRepository.java
+++ library/src/main/java/com/library/repositories/BookRepository.java
@@ -2,10 +2,17 @@ package com.library.repositories;
 
 import com.library.models.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    @Query("SELECT b FROM Book b WHERE b.name LIKE %:nameBook%")
+    List<Book> searchByName (@Param("nameBook") String nameBook);
 
 
 }
--- library/src/main/java/com/library/services/BookService.java
+++ library/src/main/java/com/library/services/BookService.java
@@ -1,12 +1,28 @@
 package com.library.services;
 
+import com.library.models.Book;
 import com.library.repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BookService {
 
     @Autowired
     private BookRepository bookRepository;
+
+    public void saveBook(Book book) {
+        bookRepository.save(book);
+    }
+
+    public void deleteBook(Book book) {
+        bookRepository.delete(book);
+    }
+
+    public List<Book> searchByName(String nameBook) {
+        return bookRepository.searchByName(nameBook);
+    }
+
 }
