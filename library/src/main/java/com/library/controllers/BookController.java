ff90bc00a257d320d412b6796cbe5c2b270b68bd
Inathan <inathan.moreira.10@gmail.com>
2021-07-05 10:55:15 -0300

    Book listings on the Home screen and navigation between screens

--- library/src/main/java/com/library/controllers/BookController.java
+++ library/src/main/java/com/library/controllers/BookController.java
@@ -5,11 +5,8 @@ import com.library.models.Book;
 import com.library.services.BookService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 //CLASSE CONTROLLER PARA BOOK(LIVRO)
 @Slf4j //Faz o log da classe para poder tratar erros
--- library/src/main/java/com/library/controllers/IndexController.java
+++ library/src/main/java/com/library/controllers/IndexController.java
@@ -1,17 +1,31 @@
 package com.library.controllers;
 
+import com.library.models.Book;
+import com.library.services.BookService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 //CLASSE CONTROLLER DO INDEX DA APLICACAO
 @Slf4j //Faz o log da classe para poder tratar erros
 @Controller
 public class IndexController {
 
+    @Autowired
+    BookService bookService;
+
     //MÉTODO PARA RETORNAR PAGINA
     @RequestMapping("/Home")
-    public String index() {
-        return "index";
+    public ModelAndView index() {
+        ModelAndView modelAndView = new ModelAndView("index");
+        List<Book> books = bookService.findAll();
+        modelAndView.addObject("books", books);
+
+        return modelAndView;
     }
+
 }
